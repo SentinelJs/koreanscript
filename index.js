e1a6be7ef45b5a68b0a20008fc0c5ace4f4d60da
unknown <yosamo9@naver.com>
2021-07-26 23:06:38 +0900

    updata

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
-class KoreaScript {
+class KoreanScript {
     constructor() {
 
     }
 
     compile(source) {
-        const openedCheckStack = [];
         const closedCheckStack = [];
         const chars = {
-            "]": "[",
-            "}": "{",
-            ")": "(",
-            "\"": "\"",
-            "'": "'"
+            "ㅑ": "ㅕ",
+            "ㅏ": "ㅓ"
         }
 
         for (let word of source.split('')) {
-            /*
-            []
-            ()
-            {}
-            ''
-            ""
-            */
-            if (!"[]{}()\"'".includes(word)) continue;
+            if (!"ㅑㅕㅏㅓ".includes(word)) continue;
             // 여닫이 제외 막기
 
-            if ("[{(\"'".includes(word)) {
+            if ("ㅓㅕ".includes(word)) {
                 closedCheckStack.push(word)
             } else {
                 const lastWord = closedCheckStack[closedCheckStack.length - 1];
 
                 if (lastWord == chars[word]) closedCheckStack.pop()
                 // 열고 닫히는게 연속됬다면 스택에서 삭제.
-                else if (lastWord !== chars[word]) throw new Error(`${lastWord} is not closed.`)
+                else if (lastWord !== chars[word]) 
+                    throw new Error(`${lastWord ?? word}가 안 ${lastWord ? "닫혔어요 !" : "열렸어요 !"}`)
                 // 스택 마지막 글자와 현재 글자가 여닫는게 다르다면 오류 발생.
             }
         }
 
         if (closedCheckStack[0]) {
-            throw new Error(`${closedCheckStack[0]} is not closed.`)
+            throw new Error(`${closedCheckStack[0]}가 안 닫혔어요 !`)
             // 스택이 남아있다면 오류 발생
         }
+
+        const sourceSplit = source.split('')
     }
 }
 
-const ks = new KoreaScript()
-ks.compile("['hello']\'")
+const ks = new KoreanScript()
+ks.compile("ㅓ")
